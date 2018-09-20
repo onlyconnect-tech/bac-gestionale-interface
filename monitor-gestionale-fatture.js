312f135d1ec3bad17b58097293633c22c6f31ac5
Massimo Cappellano <Massimo.Cappe@gmail.com>
2018-09-20 16:52:39 +0200

    improv

--- monitor-gestionale-fatture.js
+++ monitor-gestionale-fatture.js
@@ -1,30 +1,54 @@
 
 import Parser from 'node-dbf';
 
+const logger = require('./config/winston.js');
+
+import Mongo from './lib/mongo';
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+// 'myproject'
+const dbName = 'myproject';
+
 var numRow = 0;
 let parser = new Parser('./data/TABFST01.DBF');
- 
-parser.on('start', (p) => {
-    console.log('dBase file parsing has started');
-});
- 
-parser.on('header', (h) => {
-    console.log('dBase file header has been parsed' + JSON.stringify(h));
-});
- 
-parser.on('record', (record) => {
-    numRow ++;
-    //console.log( JSON.stringify(record)); 
-    doInsertRecord(record);
-});
- 
-parser.on('end', (p) => {
-    console.log('Finished parsing the dBase file - numRow: ' + numRow);
-});
- 
-parser.parse();
 
-function doInsertRecord(record) {
+const mongo = new Mongo();
+
+var dbMongo;
+
+mongo.getDB(url, dbName).then((db) => {
+    dbMongo = db;
+
+    parser.on('start', (p) => {
+        console.log('dBase file parsing has started');
+    });
+     
+    parser.on('header', (h) => {
+        console.log('dBase file header has been parsed' + JSON.stringify(h));
+    });
+     
+    parser.on('record', (record) => {
+        numRow ++;
+        //console.log( JSON.stringify(record)); 
+        doInsertRecord(db, record);
+    });
+     
+    parser.on('end', (p) => {
+        console.log('Finished parsing the dBase file - numRow: ' + numRow);
+    });
+     
+    parser.parse();
+
+}, (err) => {
+    console.log("ERROR:", err);
+});
+ 
+
+
+function doInsertRecord(db, record) {
     var seqNumberGest = record['@sequenceNumber'];
     var idFattura = record.NUMDOC;
     var annDoc = record.ANNDOC;
@@ -33,6 +57,17 @@ function doInsertRecord(record) {
     var totImp = record.TOTIMP;
     var totIVA = record.TOTIVA;
     var isDeleted = record['@deleted'];
+
+    var fattura = {
+        seqNumberGest: seqNumberGest,
+        idFattura: idFattura,
+        annDoc: annDoc,
+        datDoc: datDoc,
+        codCliente: codCliente,
+        totImp: totImp,
+        totIVA: totIVA,
+        isDeleted: isDeleted
+    };
 
     /*
     console.log("-idFattura: %d - addDoc: %s - datDoc: %s - codCliente: %d - totImp: %f - totIVA: %f - isDeleted:  %s", 
@@ -43,7 +78,7 @@ function doInsertRecord(record) {
 
     if(!isDeleted) {
 
-        insertFatture( idFattura, seqNumberGest, annDoc, datDoc, codCliente, totImp, totIVA);
+        mongo.insertFattura( db, fattura);
 
         /*
 
