ebf953a16ef1bb5e2d3b450d2f263a74ef60f7fb
Massimo Cappellano <Massimo.Cappe@gmail.com>
2018-09-21 17:39:26 +0200

    formatting

--- start-monitor-gestionale.js
+++ start-monitor-gestionale.js
@@ -3,9 +3,11 @@ require = require("esm")(module/*, options*/);
 
 // module.exports = [require("./monitor-gestionale-anagrafica.js"), require("./monitor-gestionale-fatture.js")]
 
-// module.exports = [require("./monitor-gestionale-fatture.js")]
+// module.exports = require("./monitor-gestionale-fatture.js")
 
 exports.module = {
+    
     monitorAnagrafica: require("./monitor-gestionale-anagrafica.js"),
     monitorFatture: require("./monitor-gestionale-fatture.js"),
 }
+
